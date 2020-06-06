3bb5e803c520766df3d3308b1412efc3570f8d5a
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-06 16:23:28 -0300

    Version with interface and abstract class.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,8 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class Program {
@@ -13,8 +10,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		
-		Shape s1 = new Rectangle(Color.RED, 3.0, 4.0);
-		Shape s2 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s1 = new Rectangle(Color.RED, 3.0, 4.0);
+		AbstractShape s2 = new Circle(Color.BLACK, 2.0);
 		
 		System.out.println("Circle color: " + s2.getColor());
 		System.out.println("Circle area: " + String.format("%.2f", s2.area()));
